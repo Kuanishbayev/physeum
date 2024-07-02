af4f566cb5f69248697c4ac3ccd208d644d07d58
Ernazar <kuanishbayevyernazar@gmail.com>
2024-07-02 07:18:52 +0500

    New questions added.

---
+++ src/data/db-old.js
@@ -0,0 +1,111 @@
+export const quiz = {
+    topic: 'Javascript',
+    perQuestionScore: 5,
+    questions: [
+        {
+            question: 'JavaScript-te qaysı metod JSON-dı string maǵlıwmat túrine konvertaciyalaw ushın isletiledi?',
+            choices: ['stringify()', 'parse()', 'convert()', 'Durıs juwap joq'],
+            correctAnswer: 'stringify()',
+        },
+        {
+            question: 'JavaScript tilinde qaysı gilt sóz ózgeriwshini járiyalawda isletiledi?',
+            choices: ['var', 'let', 'var hám let', 'Durıs juwap joq'],
+            correctAnswer: 'var hám let',
+        },
+        {
+            question: 'JavaScript tilinde qaysı kod dizbegi maǵlıwmattı shıǵarıw ushın isletiledi?',
+            choices: ['document.write()', 'console.log()', 'window.alert', 'Hámme juwap durıs'],
+            correctAnswer: 'Hámme juwap durıs',
+        }
+    ]
+}
+
+export const trueFalseQuiz = {
+    topic: 'True or False',
+    perQuestionScore: 5,
+    questions: [
+        {
+            question: '4 + 4 = 8',
+            choices: ['Durıs', 'Qáte'],
+            correctAnswer: 'Durıs',
+        },
+        {
+            question: 'Apple = Shiye',
+            choices: ['Durıs', 'Qáte'],
+            correctAnswer: 'Qáte',
+        },
+        {
+            question: '"1" === 1',
+            choices: ['Durıs', 'Qáte'],
+            correctAnswer: 'Qáte',
+        }
+    ]
+}
+
+export const moreButtonsQuiz = {
+    topic: 'More buttons quiz',
+    perQuestionScore: 5,
+    questions: [
+        {
+            question: 'Olar bizlerdiń joybarımızǵa __.',
+            choices: ['qızıqtı', 'alma', 'effektiv', 'aytıw', 'kompyuter', 'hawa', 'aynalar', 'port', 'úyretiw', 'forma'],
+            correctAnswer: 'qızıqtı',
+        },
+        {
+            question: 'Sálem __!',
+            choices: ['dúnya', 'bala', 'futbol', 'top', 'muz', 'klaviatura', 'at'],
+            correctAnswer: 'dúnya',
+        }
+    ]
+}
+
+export const imageQuiz = {
+    topic: 'Image quiz',
+    perQuestionScore: 5,
+    questions: [
+        {
+            image: "Bill-Gates-2011.png",
+            question: 'Súwrettegi adamnıń atı familiyası durıs keltirilgen variant qaysı?',
+            choices: ['Stiv Jobs', 'Ilon Mask', 'Bill Geyts', 'Durıs juwap joq'],
+            correctAnswer: 'Bill Geyts',
+        },
+        {
+            image: "elon-musk.webp",
+            question: 'Súwrettegi adamǵa tiyisli social tarmaq qaysı?',
+            choices: ['Instagram', 'X', 'Facebook', 'VK'],
+            correctAnswer: 'X',
+        }
+    ]
+}
+
+export const matchingPairWordsQuiz = {
+    topic: 'Matching pair words quiz',
+    perQuestionScore: 5,
+    questions: [
+        {
+            "Good": "Jaqsı",
+            "Ball": "Top",
+            "Smart": "Aqıllı",
+            "Ice": "Muz",
+        },
+        {
+            "Cat": "Pıshıq",
+            "Foot": "Ayaq",
+            "Dog": "Iyt",
+            "Bat": "Jarǵanat",
+        },
+        {
+            "Chalk": "Por",
+            "Tea": "Chay",
+            "Clever": "Aqıllı",
+            "Bag": "Sumka",
+        },
+    ]
+}
+
+export const accounts = [
+    {
+        login: "admin@example.com",
+        password: "12345678"
+    }
+]
